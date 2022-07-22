2ac997aea59372c6e685be08449f4635ae149427
jrosales90 <juan.rosales.fuentes@gmail.com>
2022-07-22 17:45:09 -0500

    feat(socket): sockets updated

--- coffee-back-demo/app.py
+++ coffee-back-demo/app.py
@@ -1,4 +1,5 @@
 from threading import Timer
+import random
 
 import eventlet
 import socketio
@@ -23,13 +24,37 @@ def disconnect(sid):
     print('disconnect ', sid)
 
 
+def variation(c_v, start, end, round_precision):
+    x = round(random.uniform(start, end), round_precision)
+
+    s_r = random.choice(['+', '-'])
+
+    c_v = c_v + x if s_r == '+' else c_v - x
+
+    return c_v
+
+
 @sio.on("live")
 def live(sid, test):
-    counter = 3
-    print('live')
-    for _ in range(100):
-        counter += 0.1
-        sio.emit('news', counter)
+    temperature = 90.00
+    pressure = 9.00
+    flow = 1.000
+    weight = 0
+
+    for in_x in range(1000000):
+        temperature = variation(temperature, 0.00, 0.05, 2)
+        pressure = variation(pressure, 0.00, 0.01, 2)
+        flow = variation(flow, 0.000, 0.001, 3)
+        weight = variation(weight, 0, 1, 1);
+
+        if weight > 60 or weight < 0: 
+            weight = 0
+
+        sio.emit('temperature', temperature)
+        sio.emit('pressure', pressure)
+        sio.emit('flow', flow)
+        sio.emit('weight', weight)
+        
         sio.sleep(0.1)
 
 
@@ -37,3 +62,8 @@ if __name__ == '__main__':
     eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
 
 
+# 90.00 variación 0.05 temperatura
+# 9.00 variación 0.01 presión
+# 1.000 variación 0.001 flujo
+
+# 0, 60 if 60 -> 0 peso
--- coffee-ui-demo/src/pages/Home/index.tsx
+++ coffee-ui-demo/src/pages/Home/index.tsx
@@ -1,29 +1,47 @@
 import React, { useEffect, useState } from "react";
-import { Pressure } from "../../Components/Pressure/Pressure";
+// import { Pressure } from "../../Components/Pressure/Pressure";
 import { io } from "socket.io-client";
+import { roundPrecision } from "../../Utils";
 import "../../index.css";
 
 const socket = io("http://0.0.0.0:5000");
 
 export default function () {
-  const [pValue, setPValue] = useState<number>(3);
+  const [temperature, setTemperature] = useState<number>(90.0);
+  const [pressure, setPressure] = useState<number>(9.0);
+  const [flow, setFlow] = useState<number>(1.0);
+  const [weight, setWeight] = useState<number>(0);
 
   useEffect(() => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    socket.on("news", function (data: any) {
-      setPValue(data);
+    socket.on("pressure", function (data: any) {
+      setPressure(data);
+    });
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    socket.on("temperature", function (data: any) {
+      setTemperature(data);
+    });
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    socket.on("flow", function (data: any) {
+      setFlow(data);
+    });
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    socket.on("weight", function (data: any) {
+      setWeight(data);
     });
   }, []);
 
   const request = () => {
-    socket.emit(
-      "send-get",
-      "demo",
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      (err: any, res: any) => {
-        console.log(res);
-      }
-    );
+    setInterval(() => {
+      socket.emit(
+        "send-get",
+        Math.random(),
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        (err: any, res: any) => {
+          console.log(res);
+        }
+      );
+    }, 100);
   };
 
   const request2 = () => {
@@ -32,13 +50,20 @@ export default function () {
 
   return (
     <div>
-      <div className="main">
+      {/* <div className="main">
         <div className="frame">
           <div className="header">
-            <Pressure pressureValue={pValue} />
+            <Pressure pressureValue={pressure} />
+            <Pressure pressureValue={temperature} />
+            <Pressure pressureValue={other} />
           </div>
         </div>
-      </div>
+      </div> */}
+      <p>Pressure: {roundPrecision(pressure, 2)}</p>
+      <p>Temperature: {roundPrecision(temperature, 2)}</p>
+      <p>Flow: {roundPrecision(flow, 3)}</p>
+      <p>Weight: {roundPrecision(weight, 0)}</p>
+
       <button
         type="button"
         onClick={() => {
